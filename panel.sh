ca5f0cbfc3c9a2957b99bbedf88424cfb529d769
Marcin Kallas <admin@sproject.pl>
2020-10-22 00:31:13 +0200

    dodanie kolorów

--- panel.sh
+++ panel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo Witaj w panelu instalatora. Co chcesz zrobić?
+echo \e[34mWitaj w panelu instalatora. Co chcesz zrobić?
 echo 1. Zainstalowac dockera
 read -p ': ' option
 case $option in
