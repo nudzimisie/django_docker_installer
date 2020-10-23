a0a690f0f884525a0bdaab0d762ee70bd27297c9
Marcin Kallas <admin@sproject.pl>
2020-10-23 03:25:16 +0200

    rc

--- python_install.sh
+++ python_install.sh
@@ -15,6 +15,6 @@ python() { python3 "$@"; }
 export -p python
 pip() { pip3 "$@"; }
 export -p pip
-pip install -r $PWD/requirements.txt
+pip install -r $PWD/django_docker_installer/requirements.txt
 echo Python i wszystkie niezbędne składniki zaostały zainstalowane 
 read -r -s -p $'\e[94mnacisnij enter by kontynuowac\n\e[39m'
