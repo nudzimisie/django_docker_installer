70cc9b366d53003938c10e9f7f7b3541a95881b1
Marcin Kallas <admin@sproject.pl>
2020-10-23 02:18:01 +0200

    korekta drobnego błędu

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ if command cat requirements.txt >/dev/null
 then
 if ! command pip install -r requirements.txt >/dev/null
 then
-echo -e "\e[41m Nie można zainstalować pakietów python. \n Możliwe, że nie ma zainstalowanego instalatora pip. \n Przeprowadź procedurę instalacyjną python. \e[39m"
+echo -e "\e[41m Nie można zainstalować pakietów python. \n Możliwe, że nie ma zainstalowanego instalatora pip. \n Przeprowadź procedurę instalacyjną python. \e[49m"
 fi
 fi
